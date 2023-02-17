606e718624175bc083a8b932f732688e6076225f
Remy Wang <remy.wang727@gmail.com>
2023-02-17 02:00:22 -0500

    Feat: add Ruby solution

---
+++ algorithms/ruby/0783-minimum-distance-between-bst-nodes.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# Problem: 783. Minimum Distance Between BST Nodes
+# URL: https://leetcode.com/problems/minimum-distance-between-bst-nodes
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def min_diff_in_bst(root)
+  @prev = nil
+  @min = nil
+  inorder_traversal(root)
+  @min
+end
+
+def inorder_traversal(root)
+  return if root.nil?
+  inorder_traversal(root.left)
+  if @prev
+    val = root.val - @prev
+    @min = @min ? [@min, val].min : val
+  end
+  @prev = root.val
+  inorder_traversal(root.right)
+end
