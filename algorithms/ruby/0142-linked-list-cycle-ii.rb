43e370f04fcb8150572604dd78166d154863be74
Remy Wang <remy.wang727@gmail.com>
2023-03-09 02:21:31 -0500

    Feat: add Ruby solution

---
+++ algorithms/ruby/0142-linked-list-cycle-ii.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+# Problem: 142. Linked List Cycle II
+# URL: https://leetcode.com/problems/linked-list-cycle-ii
+
+=begin
+
+Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+Do not modify the linked list.
+
+Constraints:
+
+* The number of the nodes in the list is in the range [0, 104].
+* -105 <= Node.val <= 105
+* pos is -1 or a valid index in the linked-list.
+
+=end
+
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} head
+# @return {ListNode}
+def detectCycle(head)
+  hash = {}
+  pos = 0
+  tmp = head
+  while tmp
+    if hash[tmp]
+      return tmp
+    else
+      hash[tmp] = pos
+    end
+
+    pos += 1
+    tmp = tmp.next
+  end
+end
