4686c022a98dbc9bb94242a459aaacc0d3f39c19
Star <remy.wang727@gmail.com>
2023-03-31 14:01:32 -0400

    Fix: update 1444 Ruby solution

--- algorithms/ruby/1444-number-of-ways-of-cutting-a-pizza.rb
+++ algorithms/ruby/1444-number-of-ways-of-cutting-a-pizza.rb
@@ -34,74 +34,49 @@ Output: 1
 
 =end
 
+# Runtime: 200 ms
+# Memory: 211.4 MB
 # @param {String[]} pizza
 # @param {Integer} k
 # @return {Integer}
 def ways(pizza, k)
-  rows = pizza.size
-  cols = pizza[0].size
-  apples = Array.new(rows + 1) { Array.new(cols + 1, 0) }
-  f = Array.new(rows) { Array.new(cols, 0) }
-
-  row = rows - 1
-  while row >= 0
-    col = cols - 1
-    while col >= 0
-      apples[row][col] = ((pizza[row][col] == "A") && 1 || 0) + apples[row + 1][col] + apples[row][col + 1] - apples[row + 1][col + 1]
-      f[row][col] = (apples[row][col] > 0) && 1 || 0
-      col -= 1
+  r, c = pizza.size, pizza[0].size
+  ap = Array.new(r + 1).map { Array.new(c + 1, 0) }
+  (0...r).reverse_each do |i|
+    (0...c).reverse_each do |j|
+      x = ?A == pizza[i][j] ? 1 : 0
+      ap[i][j] = x + ap[i + 1][j] + ap[i][j + 1] - ap[i + 1][j + 1]
     end
-    row -= 1
   end
-
+  dp = Array.new(k).map { Array.new(r).map { Array.new(c, 0) } }
+  dp[0] = Array.new(r).map.with_index { |_, i| Array.new(c).map.with_index { |_, j| ap[i][j] > 0 ? 1 : 0 } }
   mod = 1000000007
-  remain = 1
-  while remain < k
-    g = Array.new(rows) { Array.new(cols, 0) }
-    row = 0
-    while row < rows
-      col = 0
-      while col < cols
-        next_row = row + 1
-        while next_row < rows
-          if apples[row][col] - apples[next_row][col] > 0
-            g[row][col] += f[next_row][col] %= mod
-          end
-          next_row += 1
+  (1...k).each do |rmn|
+    (0...r).each do |i|
+      (0...c).each do |j|
+        val = 0
+        (i + 1...r).each do |ni|
+          val += dp[rmn - 1][ni][j] if ap[i][j] - ap[ni][j] > 0
         end
-
-        next_col = col + 1
-        while next_col < cols
-          if apples[row][col] - apples[row][next_col] > 0
-            g[row][col] += f[row][next_col] %= mod
-          end
-          next_col += 1
+        (j + 1...c).each do |nj|
+          val += dp[rmn - 1][i][nj] if ap[i][j] - ap[i][nj] > 0
         end
-        col += 1
+        dp[rmn][i][j] = val % mod
       end
-      row += 1
     end
-    f = g
-    remain += 1
   end
-
-  f[0][0]
+  dp[k - 1][0][0]
 end
 
 # **************** #
 #       TEST       #
 # **************** #
 
-# require "test/unit"
-# class Test_ways < Test::Unit::TestCase
-#   def test_
-#     assert_equal 3, ways(["A..", "AAA", "..."], 3)
-#     assert_equal 1, ways(["A..", "AA.", "..."], 3)
-#     assert_equal 1, ways(["A..", "A..", "..."], 1)
-#   end
-# end
-
-
-pizza = ["..A.A.AAA...AAAAAA.AA..A..A.A......A.AAA.AAAAAA.AA", "A.AA.A.....AA..AA.AA.A....AAA.A........AAAAA.A.AA.", "A..AA.AAA..AAAAAAAA..AA...A..A...A..AAA...AAAA..AA", "....A.A.AA.AA.AA...A.AA.AAA...A....AA.......A..AA.", "AAA....AA.A.A.AAA...A..A....A..AAAA...A.A.A.AAAA..", "....AA..A.AA..A.A...A.A..AAAA..AAAA.A.AA..AAA...AA", "A..A.AA.AA.A.A.AA..A.A..A.A.AAA....AAAAA.A.AA..A.A", ".AA.A...AAAAA.A..A....A...A.AAAA.AA..A.AA.AAAA.AA.", "A.AA.AAAA.....AA..AAA..AAAAAAA...AA.A..A.AAAAA.A..", "A.A...A.A...A..A...A.AAAA.A..A....A..AA.AAA.AA.AA.", ".A.A.A....AAA..AAA...A.AA..AAAAAAA.....AA....A....", "..AAAAAA..A..A...AA.A..A.AA......A.AA....A.A.AAAA.", "...A.AA.AAA.AA....A..AAAA...A..AAA.AAAA.A.....AA.A", "A.AAAAA..A...AAAAAAAA.AAA.....A.AAA.AA.A..A.A.A...", "A.A.AA...A.A.AA...A.AA.AA....AA...AA.A..A.AA....AA", "AA.A..A.AA..AAAAA...A..AAAAA.AA..AA.AA.A..AAAAA..A", "...AA....AAAA.A...AA....AAAAA.A.AAAA.A.AA..AA..AAA", "..AAAA..AA..A.AA.A.A.AA...A...AAAAAAA..A.AAA..AA.A", "AA....AA....AA.A......AAA...A...A.AA.A.AA.A.A.AA.A", "A.AAAA..AA..A..AAA.AAA.A....AAA.....A..A.AA.A.A...", "..AA...AAAAA.A.A......AA...A..AAA.AA..A.A.A.AA..A.", ".......AA..AA.AAA.A....A...A.AA..A.A..AAAAAAA.AA.A", ".A.AAA.AA..A.A.A.A.A.AA...AAAA.A.A.AA..A...A.AAA..", "A..AAAAA.A..A..A.A..AA..A...AAA.AA.A.A.AAA..A.AA..", "A.AAA.A.AAAAA....AA..A.AAA.A..AA...AA..A.A.A.AA.AA", ".A..AAAA.A.A.A.A.......AAAA.AA...AA..AAA..A...A.AA", "A.A.A.A..A...AA..A.AAA..AAAAA.AA.A.A.A..AA.A.A....", "A..A..A.A.AA.A....A...A......A.AA.AAA..A.AA...AA..", ".....A..A...A.A...A..A.AA.A...AA..AAA...AA..A.AAA.", "A...AA..A..AA.A.A.AAA..AA..AAA...AAA..AAA.AAAAA...", "AA...AAA.AAA...AAAA..A...A..A...AA...A..AA.A...A..", "A.AA..AAAA.AA.AAA.A.AA.A..AAAAA.A...A.A...A.AA....", "A.......AA....AA..AAA.AAAAAAA.A.AA..A.A.AA....AA..", ".A.A...AA..AA...AA.AAAA.....A..A..A.AA.A.AA...A.AA", "..AA.AA.AA..A...AA.AA.AAAAAA.....A.AA..AA......A..", "AAA..AA...A....A....AA.AA.AA.A.A.A..AA.AA..AAA.AAA", "..AAA.AAA.A.AA.....AAA.A.AA.AAAAA..AA..AA.........", ".AA..A......A.A.AAA.AAAA...A.AAAA...AAA.AAAA.....A", "AAAAAAA.AA..A....AAAA.A..AA.A....AA.A...A.A....A..", ".A.A.AA..A.AA.....A.A...A.A..A...AAA..A..AA..A.AAA", "AAAA....A...A.AA..AAA..A.AAA..AA.........AA.AAA.A.", "......AAAA..A.AAA.A..AAA...AAAAA...A.AA..A.A.AA.A.", "AA......A.AAAAAAAA..A.AAA...A.A....A.AAA.AA.A.AAA.", ".A.A....A.AAA..A..AA........A.AAAA.AAA.AA....A..AA", ".AA.A...AA.AAA.A....A.A...A........A.AAA......A...", "..AAA....A.A...A.AA..AAA.AAAAA....AAAAA..AA.AAAA..", "..A.AAA.AA..A.AA.A...A.AA....AAA.A.....AAA...A...A", ".AA.AA...A....A.AA.A..A..AAA.A.A.AA.......A.A...A.", "...A...A.AA.A..AAAAA...AA..A.A..AAA.AA...AA...A.A.", "..AAA..A.A..A..A..AA..AA...A..AA.AAAAA.A....A..A.A"]
-k = 8
-puts ways(pizza, k) # 641829390
+require "test/unit"
+class Test_ways < Test::Unit::TestCase
+  def test_
+    assert_equal 3, ways(["A..", "AAA", "..."], 3)
+    assert_equal 1, ways(["A..", "AA.", "..."], 3)
+    assert_equal 1, ways(["A..", "A..", "..."], 1)
+  end
+end
