c73c5697aadfda2a17ff581c52840531f64b05a7
Remy Wang <remy.wang727@gmail.com>
2023-05-08 10:13:58 -0400

    236. Lowest Common Ancestor of a Binary Tree

---
+++ algorithms/ruby/0236-lowest-common-ancestor-of-a-binary-tree.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+# 236. Lowest Common Ancestor of a Binary Tree
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree
+# Medium
+
+=begin
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Example 1:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+
+Example 2:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+
+Example 3:
+Input: root = [1,2], p = 1, q = 2
+Output: 1
+
+Constraints:
+The number of nodes in the tree is in the range [2, 105].
+-109 <= Node.val <= 109
+All Node.val are unique.
+p != q
+p and q will exist in the tree.
+=end
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @param {TreeNode} p
+# @param {TreeNode} q
+# @return {TreeNode}
+def lowest_common_ancestor(root, p, q)
+  return root if [nil, p, q].index root
+  left = lowest_common_ancestor(root.left, p, q)
+  right = lowest_common_ancestor(root.right, p, q)
+  left && right ? root : left || right
+end
