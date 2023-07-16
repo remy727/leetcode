ff0e172cb2558a36f67ba539e53aedb85b73d600
Remy Wang <remy.wang727@gmail.com>
2023-07-16 10:57:57 -0400

    1125. Smallest Sufficient Team

---
+++ algorithms/ruby/1125-smallest-sufficient-team.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+# 1125. Smallest Sufficient Team
+# Hard
+# https://leetcode.com/problems/smallest-sufficient-team
+
+=begin
+In a project, you have a list of required skills req_skills, and a list of people. The ith person people[i] contains a list of skills that the person has.
+Consider a sufficient team: a set of people such that for every required skill in req_skills, there is at least one person in the team who has that skill. We can represent these teams by the index of each person.
+* For example, team = [0, 1, 3] represents the people with skills people[0], people[1], and people[3].
+Return any sufficient team of the smallest possible size, represented by the index of each person. You may return the answer in any order.
+It is guaranteed an answer exists.
+
+Example 1:
+Input: req_skills = ["java","nodejs","reactjs"], people = [["java"],["nodejs"],["nodejs","reactjs"]]
+Output: [0,2]
+
+Example 2:
+Input: req_skills = ["algorithms","math","java","reactjs","csharp","aws"], people = [["algorithms","math","java"],["algorithms","math","reactjs"],["java","csharp","aws"],["reactjs","csharp"],["csharp","math"],["aws","java"]]
+Output: [1,2]
+
+Constraints:
+* 1 <= req_skills.length <= 16
+* 1 <= req_skills[i].length <= 16
+* req_skills[i] consists of lowercase English letters.
+* All the strings of req_skills are unique.
+* 1 <= people.length <= 60
+* 0 <= people[i].length <= 16
+* 1 <= people[i][j].length <= 16
+* people[i][j] consists of lowercase English letters.
+* All the strings of people[i] are unique.
+* Every skill in people[i] is a skill in req_skills.
+* It is guaranteed a sufficient team exists.
+=end
+
+# @param {String[]} req_skills
+# @param {String[][]} people
+# @return {Integer[]}
+def smallest_sufficient_team(req_skills, people)
+  n = req_skills.size
+  skill_map, dp = {}, Array.new(1 << n, [n + 1]) # Initialize.
+
+  # Map skills to index.
+  req_skills.each_with_index { |rs, idx| skill_map[rs] = idx }
+
+  # Create bitmask for each person representing his skills
+  people = people.map { |p| p.inject(0) { |acc, skill| acc | (1 << skill_map[skill]) } }
+
+  # For covering no skills, 0 member is enough although having 0 members are not possible
+  dp[0] = [0, []]
+
+  people.each_with_index do |skills, p|
+    ((1 << req_skills.length) - 1).downto(0) do |i|
+      # skills obtained by adding new person
+      nex = i | skills
+      # if new person adds any new skill then we add him to team and update team size and new team in dp table
+      if dp[nex][0] > dp[i][0] + 1
+        dp[nex] = [dp[i][0] + 1, dp[i][1] + [p]]
+      end
+    end
+  end
+
+  # return the team
+  dp[(1 << req_skills.size) - 1][1]
+end
+
+# **************** #
+#       TEST       #
+# **************** #
+
+require "test/unit"
+class Test_smallest_sufficient_team < Test::Unit::TestCase
+  def test_
+    assert_equal [0, 2], smallest_sufficient_team(["java", "nodejs", "reactjs"], [["java"], ["nodejs"], ["nodejs", "reactjs"]])
+    assert_equal [1, 2], smallest_sufficient_team(["algorithms", "math", "java", "reactjs", "csharp", "aws"], [["algorithms", "math", "java"], ["algorithms", "math", "reactjs"], ["java", "csharp", "aws"], ["reactjs", "csharp"], ["csharp", "math"], ["aws", "java"]])
+  end
+end
